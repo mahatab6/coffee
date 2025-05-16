6ba29a76563a8c40e9328ef79fc4f366572bfdec
mahatab6 <mahatabcall56@gmail.com>
2025-05-16 10:06:06 +0600

    sing in section added

--- src/pages/Navber.jsx
+++ src/pages/Navber.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import navbg from '../assets/images/more/15.jpg'
 import logo from '../assets/images/more/logo1.png'
+import { Link } from 'react-router';
 
 const Navber = () => {
     return (
         <div>
           <div className="flex items-center justify-between bg-base-100 shadow-sm px-4" style={{ background: `url(${navbg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <div className="w-[50px]"></div>
-            <div className="flex items-center space-x-2 mx-auto">
+            <div className=" space-x-2 mx-auto">
+                <Link to="/" className='flex items-center'>
                 <img className='w-[50px]' src={logo} alt="" />
                 <a className="text-white text-2xl">Espresso Emporium</a>
+                </Link>
             </div>
-            <button className='btn btn-sm '>Sign-In</button>
+            <Link to="/sing-in"><button className='btn btn-sm '>Sign-In</button></Link>
             </div>
 
         </div>
